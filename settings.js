fd004ccc46cd2b85e8a314152396874cba331492
Up.gg <Brage@reinaas.no>
2025-09-23 16:22:35 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const openBtn = document.querySelector(".create-account-btn");
+  const closeBtn = document.getElementById("closeSettings");
+  const frame = document.getElementById("settingsFrame");
+
+  openBtn.addEventListener("click", () => {
+    frame.style.display = "block"; // Show the frame
+  });
+
+  closeBtn.addEventListener("click", () => {
+    frame.style.display = "none"; // Hide the frame
+  });
+});
+
+
